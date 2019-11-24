636093c650a74634081f5b41a00def4c1522c99c
Kathy Daring <helgatheviking@gmail.com>
2019-11-24 15:29:32 -0700

    clone add to cart button into mobile footer

--- assets/js/wc-mnm-mobile-styles.js
+++ assets/js/wc-mnm-mobile-styles.js
@@ -1,4 +1,4 @@
-jQuery( document ).ready( function( $ ) {
+;( function( $ ) {
 
 	$.fn.isInViewport = function() {
 		var elementTop = $(this).offset().top;
@@ -6,15 +6,35 @@ jQuery( document ).ready( function( $ ) {
 		var viewportBottom = viewportTop + $(window).height();return elementBottom > viewportTop && elementTop < viewportBottom;
 	};
 
-	$(window).on('resize scroll', function() {
-	
-		if( $( "form.mnm_form .products" ).isInViewport() ) {
-			$( "form.mnm_form" ).find('.mnm_message').addClass('fixed');
-		} else {
-			$( "form.mnm_form" ).find('.mnm_message').removeClass('fixed');
-		}
-	});
+	// Hook into Mix and Match.
+	$( '.mnm_form' )
+		.on( 'wc-mnm-initializing', function( event, container ) {
+			
+			// Duplicate the add to cart button.
+			var $cloned_button = container.$mnm_button.clone( true );
+			container.$mnm_message_content.after( $cloned_button );
 
-} );
+			var $products = container.$mnm_form.find( '.products' ).first();
+			var $scroll_selector = $products.length ? $products : container.$mnm_form.find( 'table tbody' );
+
+			$(window).on( 'resize scroll', function() {
+				if( $scroll_selector.length && $scroll_selector.isInViewport() ) {
+					container.$mnm_data.toggleClass( 'fixed', true );
+					container.$mnm_message.toggleClass( 'woocommerce-info', false );
+				} else {
+					container.$mnm_data.toggleClass( 'fixed', false );
+					container.$mnm_message.toggleClass( 'woocommerce-info', true );
+				}
+			});
 
 
+		} )
+		.on( 'wc-mnm-hide-add-to-cart-button', function( event, container ) {
+			container.$mnm_data.toggleClass( 'valid', false );
+		} )
+		.on( 'wc-mnm-display-add-to-cart-button', function( event, container ) {
+			container.$mnm_data.toggleClass( 'valid', true );
+		} );
+
+
+} ) ( jQuery );

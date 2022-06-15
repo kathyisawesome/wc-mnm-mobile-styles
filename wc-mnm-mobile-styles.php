08cee700b942445b72f64a22efb8faaf5fbf2c5c
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2022-06-15 17:30:04 -0600

    bump tested versions

--- wc-mnm-mobile-styles.php
+++ wc-mnm-mobile-styles.php
@@ -11,10 +11,10 @@
 * Domain Path: /languages/
 *
 * Requires at least: 5.1.0
-* Tested up to: 5.9.0
+* Tested up to: 6.0.0
 *
 * WC requires at least: 5.9.0
-* WC tested up to: 6.1.0
+* WC tested up to: 6.6.0
 *
 * GitHub Plugin URI: kathyisawesome/wc-mnm-mobile-styles
 * GitHub Plugin URI: https://github.com/kathyisawesome/wc-mnm-mobile-styles

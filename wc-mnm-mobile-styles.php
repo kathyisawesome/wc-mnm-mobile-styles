23e494f2371dca9f7bfed794340a26e3d9e791c8
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-03-08 12:09:04 -0500

    version bump 2.0.0-rc.8 (entirely refactored, so I'm not sure RC means what you think it means)

--- wc-mnm-mobile-styles.php
+++ wc-mnm-mobile-styles.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Mix and Match - Mobile Styles
  * Plugin URI: https://woocommerce.com/products/woocommerce-mix-and-match-products/
  * Description: Turns the Mix and Match counter message into a sticky footer.
- * Version: 2.0.0-rc.7
+ * Version: 2.0.0-rc.8
  * Author: Kathy Darling
  * Author URI: http://kathyisawesome.com/
  *
@@ -32,8 +32,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class WC_MNM_Mobile_Styles {
 
-	const VERSION = '2.0.0-rc.7';
 	const REQ_MNM_VERSION = '2.7.0-beta.1';
+	const VERSION = '2.0.0-rc.8';
 
 	/**
 	 * Fire in the hole!
